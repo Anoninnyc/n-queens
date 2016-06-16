cad45509c5e499b18aa30966dd8e3ebfa3bb6f6e
Hack Reactor Students <accounts@hackreactor.com>
2016-06-16 16:20:28 -0700

    complete test optimizations

--- src/solvers.js
+++ src/solvers.js
@@ -1,21 +1,22 @@
-/*  
+/*
+
 var board = [
   [0, 0, 0, 0],
+  [0, 1, 0, 0],
   [0, 0, 1, 0],
-  [0, 0, 0, 0],
-  [1, 0, 0, 0]
-]
+  [0, 0, 0, 0]
+];
 //Function to replace overbearing diagonal checker.....
 
 var test = function(n, t, l, board) {
 // 'n' is conventional 'n','t' is row index FROM the top (i.e. currColumn),'l' us column index from the left (i.e. 'i' in a for loop), board is array or arrays
 
 //lets insert a piece 
-  board[t][l] = 1
+  board[t][l] = 1;
   
   //lets find the upper-leftmost square on the board it can attack;
   var topLeft = t;
-  var leftLeft = l
+  var leftLeft = l;
   while (topLeft > 0 && leftLeft > 0 && leftLeft < n - 1) {
     topLeft--;
     leftLeft--;
@@ -23,7 +24,7 @@ var test = function(n, t, l, board) {
 
   //lets find the upper-rightmost square on the board it can attack;
   var topRight = t;
-  var leftRight = l
+  var leftRight = l;
   while (topRight > 0 && leftRight > 0 && leftRight < n - 1) {
     topRight--;
     leftRight++;
@@ -58,25 +59,13 @@ var test = function(n, t, l, board) {
 };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
         _
    ___  ___ | |_   _____ _ __ ___
   / __|/ _ \| \ \ / / _ \ '__/ __|
   \__ \ (_) | |\ V /  __/ |  \__ \
   |___/\___/|_| \_/ \___|_|  |___/
 
-*/
+
 
 // hint: you'll need to do a full-search of all possible arrangements of pieces!
 // (There are also optimizations that will allow you to skip a lot of the dead search space)
@@ -155,7 +144,8 @@ window.findNQueensSolution = function(n) {
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
   var outcomes = findAllSolutions(n);
-  return outcomes.length;
+  // return outcomes.length;
+  return outcomes;
 };
 
 
@@ -165,7 +155,7 @@ window.findAllSolutions = function(n, board, outcomes, currentRow, counter) {
 
   //Initialize outcomes array
   if (outcomes === undefined) {
-    outcomes = [];
+    outcomes = 0;
   }
 
   //If there is no board passed in, initialize an empty n x n board
@@ -183,19 +173,20 @@ window.findAllSolutions = function(n, board, outcomes, currentRow, counter) {
     counter = 0;
   }
 
-  var skippedFlag = false; 
+  // var skippedFlag = false; 
 
   //Iterate through all the column indices, while on the current row
   for (var i = 0; i < n; i++) {
     //Toggle on
     counter++;
     board.togglePiece(currentRow, i);
-    if (!board.hasAnyConflictsQueen(i) && n !== currentRow + 1) {
+    if (!board.hasAnyConflictsQueen(i, currentRow) && n !== currentRow + 1) {
       outcomes = findAllSolutions(n, board, outcomes, currentRow + 1, counter);
-    } else if (!board.hasAnyConflictsQueen(i) && currentRow + 1 === n) {
+    } else if (!board.hasAnyConflictsQueen(i, currentRow) && currentRow + 1 === n) {
       if (counter === n) {
-        var boardCopy = JSON.parse(JSON.stringify(board));
-        outcomes.push(boardCopy);
+        // var boardCopy = JSON.parse(JSON.stringify(board));
+        // outcomes.push(boardCopy);
+        outcomes++;
       }
     }
     //Toggle off
@@ -204,19 +195,21 @@ window.findAllSolutions = function(n, board, outcomes, currentRow, counter) {
     //Move onto the next column index in the current row
   }
 
-  if (!skippedFlag && n !== currentRow + 1 && board[0] !== undefined) {
-    skippedFlag = true;
-    outcomes = findAllSolutions(n, board, outcomes, currentRow + 1, counter);
-  }
+  //Skipped flag in case we want to get all possible solutions (not just with n pieces)
+  // if (!skippedFlag && n !== currentRow + 1 && board[0] !== undefined) {
+  //   skippedFlag = true;
+  //   outcomes = findAllSolutions(n, board, outcomes, currentRow + 1, counter);
+  // }
 
- // console.log('flag status:', skippedFlag, 'outcomes:', outcomes,'row:',currentRow,'board:',board);
+  //console.log('flag status:', skippedFlag, 'outcomes:', outcomes,'row:',currentRow,'board:',board);
  
   //If at the end of the row, after iterating through all columns
   //Return out of function to the previous function invocation
 
   //If statement to return entire board goes here
   if (counter === n) {
-    outcomes.push(JSON.parse(JSON.stringify(board)));
+    // outcomes.push(JSON.parse(JSON.stringify(board)));
+    outcomes++;
   }
   return outcomes;
 };
@@ -334,10 +327,10 @@ window.findOneSolutionQueen = function(n, board, outcomes, currentRow, counter) 
     //Toggle on
     counter++;
     board.togglePiece(currentRow, i);
-    if (!board.hasAnyConflictsQueen(i) && n !== currentRow + 1) {
+    if (!board.hasAnyConflictsQueen(i, currentRow) && n !== currentRow + 1) {
       outcomes = findOneSolutionQueen(n, board, outcomes, currentRow + 1, counter);
       if (outcomes[0] !== undefined) { return outcomes; }
-    } else if (!board.hasAnyConflictsQueen(i) && currentRow + 1 === n) {
+    } else if (!board.hasAnyConflictsQueen(i, currentRow) && currentRow + 1 === n) {
       if (counter === n) {
         var boardCopy = JSON.parse(JSON.stringify(board));
         outcomes.push(boardCopy);
